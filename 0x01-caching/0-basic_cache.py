df964d4539a02dbc3ff4c4a80384ab0a7815b340
mahlet <akotet2123@gmail.com>
2023-08-22 20:04:42 +0000

    basic cacheing

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+""" 0-basic_cache module """
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """
+    class BasicCache that inherits from BaseCaching
+    and is a caching system
+    """
+    def put(self, key, item):
+        """
+        assign to the dictionary self.cache_data the
+        item value for the key key.
+        """
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """
+        return the value in self.cache_data linked to key.
+        """
+        if key is None or key not in self.cache_data:
+            return None    
+        return self.cache_data[key] 
